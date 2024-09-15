31206e49cee26f2d5fdb85b5f0a745b9ad3573ef
IamPiyushChauhan <130564688+IamPiyushChauhan@users.noreply.github.com>
2024-09-15 23:34:10 +0530

    added / endpoint

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.post('/git-info', (req, res) => {
     });
 });
 
+app.get('/', (req, res) => {
+    
+    res.status(200).json({
+        message: 'working'
+    });
+});
+
 // Start the server and listen on port 3000
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
